832da6a7861812aaebcce72cb97516622a28a731
Brian Felix <fbrian100@gmail.com>
2026-03-16 21:46:45 -0400

    Update footer attribution links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,15 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Created by Brian Felix to support the <Link href="https://vroom.fun" target="_blank">$VROOM</Link> liquidity pool.
+      Created by Brian Felix (
+      <a href="farcaster.xyz/chamaquito.eth" target="_blank" rel="noopener noreferrer">
+        @chamaquito.eth
+      </a>
+      ) to support the{" "}
+      <Link href="https://vroom.fun" target="_blank">
+        $VROOM
+      </Link>{" "}
+      liquidity pool.
     </footer>
   );
 }
